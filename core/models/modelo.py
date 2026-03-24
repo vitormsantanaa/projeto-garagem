a572c04b14da8e31cf188c86e46b9548deadf30e
vitormsantanaa <vitormanoelssz1704@gmail.com>
2026-03-24 16:09:13 -0300

    Criação das chaves estrangeiras

--- core/models/modelo.py
+++ core/models/modelo.py
@@ -4,3 +4,8 @@ class Modelo(models.Model):
     nome = models.CharField(max_length=80)
     marca = models.CharField(max_length=80, blank=True, null=True)
     categoria = models.CharField(max_length=80, blank=True, null=True)
+
+    def __str__(self):
+        marca = self.marca.upper() if self.marca else ""
+        nome = self.nome.upper()
+        return f"{self.id} - {marca} - {nome}"
--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -1,5 +1,14 @@
 from django.db import models 
+from .acessorio import Acessorio
+from .cor import Cor
+from .modelo import Modelo
 
 class Veiculo(models.Model):
     ano = models.IntegerField(null=True, blank=True, default=0)
     preco = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, default=0)
+    acessorios = models.ManyToManyField(Acessorio, blank=True, related_name='veiculos')
+    cor = models.ForeignKey(Cor, on_delete=models.CASCADE, related_name='veiculos')
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE, related_name='veiculos')
+
+    def __str__(self):
+        return f"{self.id} - {self.acessorios} - {self.cor} - {self.modelo}"
